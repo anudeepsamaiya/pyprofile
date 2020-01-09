3e4e68c9bc84b54893e47859c8c39409a83baf62
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2020-01-09 16:17:59 +0530

    Bump version in setup.py.

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="pyprofile",
-    version="0.0.4",
-    author="Anudeep Samaiya",
-    author_email="anudeepsamaiya@gmail.com",
-    description="Python profiling and visualizing package.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/anudeepsamaiya/pyprofile",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+if __name__ == "__main__":
+    setuptools.setup(
+        name="pyprofile",
+        version="0.1.1",
+        author="Anudeep Samaiya",
+        author_email="anudeepsamaiya@gmail.com",
+        description="Python profiling and visualizing package.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/anudeepsamaiya/pyprofile",
+        packages=setuptools.find_packages(where="src"),
+        package_dir={"": "src"},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+    )
